5e17da495794cc78d946e79d103ff97b7b490d2d
huizhang001 <tuzisir@163.com>
2020-11-17 01:39:43 +0800

    Feat: lua脚本bug修复

--- src/Core/LuaScript.php
+++ src/Core/LuaScript.php
@@ -19,15 +19,19 @@ class LuaScript
     public static function incr()
     {
         return <<<SCRIPT
-        local nextValue=KEYS[3];
-        local value = redis.call('ZRANGEBYSCORE', KEYS[1], KEYS[2], KEYS[2]);
+        local key=KEYS[1];
+        local score=KEYS[2];
+        local incrValue=KEYS[3];
+        local value = redis.call('ZRANGEBYSCORE', key, score, score);
+        local nextValue = incrValue;
         if (type(value) == 'table' and #value > 0)
         then
-            nextValue = value[1]+KEYS[3];
-            redis.call('ZREMRANGEBYSCORE', KEYS[1], KEYS[2], KEYS[2]);
-            redis.call('ZADD', KEYS[1], KEYS[2], nextValue);
+            local lastValue = string.gsub(value[1], score .. '_', '',1)
+            nextValue = lastValue+incrValue;
+            redis.call('ZREMRANGEBYSCORE', key, score, score);
+            redis.call('ZADD', key, score, score .. '_' .. nextValue);
         else
-            redis.call('ZADD', KEYS[1], KEYS[2], nextValue);
+            redis.call('ZADD', key, score, score .. '_' .. nextValue);
         end
         return nextValue;
 SCRIPT;
@@ -43,9 +47,18 @@ SCRIPT;
     {
         return <<<SCRIPT
         local result = 0;
-        local array = redis.call('ZRANGEBYSCORE', KEYS[1], KEYS[2], KEYS[3]);
+        local key = KEYS[1];
+        local startScore = KEYS[2];
+        local endScore = KEYS[3];
+        local array = redis.call('ZRANGEBYSCORE', key, startScore, endScore);
         for i=1, #array do
-            result = result + array[i];
+            local start = string.find(array[i], '_', 1)+1; 
+            if (start == false)
+            then
+            
+            else
+                result = result + string.sub(array[i], start);
+            end
         end
         return result;
 SCRIPT;
